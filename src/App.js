d136b2af4f4dc9009f2c3c6c6d2c2c70bdaf692c
Niki Brown <nikibrown@Nikis-MacBook-Pro.local>
2018-11-23 17:31:49 -0500

    checkpoint react todo events

--- src/App.js
+++ src/App.js
@@ -11,8 +11,40 @@ class App extends Component {
                 { description: 'Wash the cat', isCompleted: true },
                 { description: 'Wash the dog', isCompleted: false },
                 { description: 'Wash the hamster', isCompleted: false }
-            ]
+            ],
+            newTodoDescription: ''
         }
+    }
+
+    handleChange(event) {
+        this.setState({ newTodoDescription: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        // prevents submit with empty value
+        if (!this.state.newTodoDescription) { return }
+
+        const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+
+        // This adds the new todo to the state. Why don't we just push this to the toDos array? Not sure I understand the spread syntax here
+        this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+   
+    }
+      
+    toggleComplete(index) {
+        // create a copy of the array - ?  I get that .slice method returns a copy of the array but  why do we use slice if we aren't actually removing values? Why not just makea copy and set it equal to a variable? 
+        const todos = this.state.todos.slice();
+        
+        // grab the index of the specific todod
+        const todo = todos[index];
+        
+        // toggle the isCompleted value
+        todo.isCompleted = todo.isCompleted ? false : true;
+        
+        // using setState method to set this.state.todos to the new todos array we created with the toggled isCompleted value
+        this.setState({ todos: todos });
     }
     render() {
         return (
@@ -20,9 +52,13 @@ class App extends Component {
                 <ul>
                     { 
                         this.state.todos.map( (todo, index) =>
-                        <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+                            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index)  } />
                     )}
                 </ul>
+                <form onSubmit={ (event) => this.handleSubmit(event) }>
+                    <input type="text" value={ this.state.newTodoDescription } onChange={ (event) => this.handleChange(event) } />
+                    <input type="submit" />
+                </form>
             </div>
         );
     }
