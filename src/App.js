3db923d7158ec7dcf18fabc0e427a79fa3606252
Niki Brown <nikibrown@Nikis-MacBook-Pro.local>
2018-11-23 19:35:38 -0500

    attempted to add deleteTodo functionality

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
         // create a copy of the array - ?  I get that .slice method returns a copy of the array but  why do we use slice if we aren't actually removing values? Why not just makea copy and set it equal to a variable? 
         const todos = this.state.todos.slice();
         
-        // grab the index of the specific todod
+        // grab the index of the specific todo
         const todo = todos[index];
         
         // toggle the isCompleted value
@@ -46,13 +46,33 @@ class App extends Component {
         // using setState method to set this.state.todos to the new todos array we created with the toggled isCompleted value
         this.setState({ todos: todos });
     }
+
+    deleteTodo(index) {
+        //console.log(index);
+        let todos = this.state.todos;
+
+        todos.filter((todo, indexofTodo) =>  {
+            console.log("index of todo that is run through filter: " + indexofTodo);
+            console.log("todo that was clicked: " + index);   
+            
+            // return the todos that do not have the index of the one clicked
+            // not working whyyyyy          
+            return indexofTodo !== index;
+        });
+
+        console.log(todos);
+
+        this.setState({ todos: todos });
+    }
+
+
     render() {
         return (
             <div className="App">
                 <ul>
                     { 
                         this.state.todos.map( (todo, index) =>
-                            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index)  } />
+                            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) }  deleteTodo={ () => this.deleteTodo(index) } />
                     )}
                 </ul>
                 <form onSubmit={ (event) => this.handleSubmit(event) }>
